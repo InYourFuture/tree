e3db13207c61c46a91e099a070a8de064aef5c1d
admin <admin@git.com>
2020-01-14 15:32:38 +0800

    二叉树

---
+++ preorderTraversal.py
@@ -0,0 +1,18 @@
+#递归实现二叉树的前序遍历
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        return [root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right)
